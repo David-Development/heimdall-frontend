c769cd0be8bdafc0ba08e82c3027e3e3bf12f829
David Luhmer <david-dev@live.de>
2017-12-02 11:01:34 +0100

    remove recognition list in liveview

--- src/LiveView.js
+++ src/LiveView.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './LiveView.css';
 import WSAvcPlayer from './h264/wsavc/WSAvcPlayer'
-import FlipMove from 'react-flip-move';
-
-import ArrowUp from './images/arrow-up.svg'
-import Logan from './images/logan.jpg'
 
 class LiveView extends Component {
 
@@ -36,25 +32,6 @@ class LiveView extends Component {
     this.wsavc.disconnect();
   }
 
-  addRecognition() {
-    let recognition_img_url = Logan //"https://n6-img-fp.akamaized.net/free-icon/up-arrow_318-74795.jpg?size=338c&ext=jpg";
-
-    //create a unike key for each new recognition item
-    var timestamp = (new Date()).getTime();
-    // update the state object
-    this.state['face-' + timestamp ] = recognition_img_url;
-
-    let keys = Object.keys(this.state);
-    let keyCount = keys.length;
-    
-    for(let i = 0; i < keyCount - 12; i++) {
-      delete this.state[keys[i]];
-      //console.log("Deleting key:", keys[i]);
-    }
-
-    this.setState(this.state); // Update ui
-  }
-
   /*
     <Button type="button" onClick={() => this.wsavc.playStream()}>Start Video</Button>
     <Button type="button" onClick={() => this.wsavc.stopStream()}>Stop Video</Button>
@@ -66,19 +43,6 @@ class LiveView extends Component {
       <div className="LiveView">
         
         <p id="identified_person_name">Test Person</p>
-        <div id="container_recognition_results">
-          <div id="container_recognition_controls">
-            <img width="100" src={ArrowUp} onClick={() => this.addRecognition()} alt="" />
-          </div>
-          
-          <FlipMove id="recognition_list" duration={150} enterAnimation="accordionHorizontal" leaveAnimation="accordionHorizontal">
-            {
-              Object.keys(this.state).reverse().map(function(key) {
-                return <img key={key} width="100" src={this.state[key]} alt="" />
-              }.bind(this))
-            }
-          </FlipMove>
-        </div>
         <canvas id="canvas_live_stream" ref="canvas_live_stream"></canvas>
       </div>
     )

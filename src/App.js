7e78ff49fce0f07f3ce33206ace582b4edc96ef3
David Luhmer <david-dev@live.de>
2018-03-26 15:33:14 +0200

    Add paddingBottom to all pages

--- src/App.js
+++ src/App.js
@@ -115,6 +115,7 @@ const styles = theme => ({
     //padding: 24,
     height: 'calc(100% - 56px)',
     marginTop: 56,
+    paddingBottom: 32,
     [theme.breakpoints.up('sm')]: {
       height: 'calc(100% - 64px)',
       marginTop: 64,
